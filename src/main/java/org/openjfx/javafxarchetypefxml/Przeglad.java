f51d6852d42e1efe4057e887c14285eb53f144d2
Piskosz <132614400+Piskosz@users.noreply.github.com>
2023-06-18 17:16:54 +0200

    api

---
+++ src/main/java/org/openjfx/javafxarchetypefxml/Przeglad.java
@@ -0,0 +1,29 @@
+package org.openjfx.javafxarchetypefxml;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Przeglad extends Application{
+ListView<String>listView;
+Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        listView = new ListView<>();
+        listView.getItems().addAll("Auta1","Auta2","Auta");
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20,20,20,20));
+        layout.getChildren().addAll(listView);
+        scene = new Scene(layout,300,250);
+
+
+    }
+}
